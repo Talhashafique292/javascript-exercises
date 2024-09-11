c6a8b1d70b8c1fccab118ead51a617371dc61ead
Talhashafique292 <talhashafique292@gmail.com>
2024-09-11 18:32:43 +0500

    Reverse string method used

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    // let str = string
+    // let strtoarray = str.split("")
+    // let reversearray= strtoarray.reverse()
+    // let arraytostr= reversearray.toString();
+    // let newstr = arraytostr.replace(/,/g, '');
+    return string.split('').reverse().join('')
 };
+
+
 
 // Do not edit below this line
 module.exports = reverseString;
