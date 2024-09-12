2b2d1de45ae3514be9a7db058520dcf57b08ec1b
Talhashafique292 <talhashafique292@gmail.com>
2024-09-12 17:27:33 +0500

    rest operator and .filter method applied

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    // let newArray = [];
+
+    // array.forEach((items) => {
+    //     if (!args.includes(items)) {
+    //         newArray.push(items);
+    //     }
+    // });
+
+    // return newArray;
+
+    // second solution is
+    return array.filter(val => !args.includes(val))
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
